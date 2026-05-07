b63b161f8484257e573a36e8ac82ee8d107d1263
Alexander Verbraeck <a.verbraeck@gmail.com>
2026-05-07 23:34:53 +0200

    Issue #4. SpeedConversions example

--- src/main/java/org/djunits/demo/examples/SpeedConversions.java
+++ src/main/java/org/djunits/demo/examples/SpeedConversions.java
@@ -7,16 +7,16 @@ import org.djunits.quantity.Mass;
 import org.djunits.quantity.Speed;
 
 /** demo for conversions. */
-public final class ExamplesSpeedConversions
+public final class SpeedConversions
 {
     /** */
-    private ExamplesSpeedConversions()
+    private SpeedConversions()
     {
         // utility constructor.
     }
 
     /**
-     * @param args args
+     * @param args not used
      */
     public static void main(final String[] args)
     {
@@ -36,25 +36,32 @@ public final class ExamplesSpeedConversions
         System.out.println("difference: " + diff); // Default display unit will be SI unit for speed
         diff.setDisplayUnit(Speed.Unit.mi_h); // Change default display unit; internal SI value is unaltered
         System.out.println("difference: " + diff);
-        System.out.println("difference: " + diff.getInUnit(SpeedUnit.KNOT) + " kt"); // Works, but not mistake-safe
-        System.out.println("difference: " + diff.toString(SpeedUnit.KNOT)); // Safer
+        System.out.println("difference: " + diff.getInUnit(Speed.Unit.kt) + " kt"); // Works, but not mistake-safe
+        System.out.println("difference: " + diff.format(Speed.Unit.kt)); // Safer
         System.out.println("difference: " + diff.si + " m/s (si)"); // Programmer must be really sure that SI-unit is m/s
         System.out.println("difference: " + diff.si() + " m/s (si)"); // Same as previous
-        System.out.println("difference: " + diff.toString(SpeedUnit.SI) + " (si)"); // Safer
-        System.out.println("difference: " + diff.toString(SpeedUnit.KM_PER_HOUR));
+        System.out.println("difference: " + diff.format(Speed.Unit.SI) + " (si)"); // Safer
+        System.out.println("difference: " + diff.format(Speed.Unit.km_h));
     }
 
     /** example 2. */
     public static void example2()
     {
-        Speed speed = new Speed(12, SpeedUnit.KM_PER_HOUR);
-        Length length = new Length(4, LengthUnit.MILE);
-        Duration howLongOK = length.divideScalar(speed); // Good
+        Speed speed = new Speed(12, Speed.Unit.km_h);
+        Length length = new Length(4, Length.Unit.mi);
+        Duration howLongOK = length.divide(speed); // Good
         System.out.println(howLongOK);
-        // Duration howLongWrong = speed.divide(length); // Does not compile; result would be a frequency
-        // Speed other = speed.minus(length); // Does not compile; subtracting a length from a speed makes no sense
-        // Acceleration acceleration = speed.times(speed).asAcceleration(); // Throws a UnitRuntimeException
-        Energy kineticEnergy = speed.times(speed).times(new Mass(3, MassUnit.KILOGRAM).times(0.5)).asEnergy(); // OK
+
+        // Duration howLongWrong = speed.divide(length);
+        // Does not compile; result would be a frequency
+
+        // Speed other = speed.minus(length);
+        // Does not compile; subtracting a length from a speed makes no sense
+
+        // Acceleration acceleration = speed.times(speed).asAcceleration();
+        // Throws a UnitRuntimeException
+
+        Energy kineticEnergy = speed.multiply(speed).multiply(new Mass(3, Mass.Unit.kg).scaleBy(0.5)).as(Energy.Unit.J); // OK
         System.out.println(kineticEnergy);
     }
 
