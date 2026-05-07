69a059aa8f5d91c0851e86c9873950077044ddc6
Alexander Verbraeck <a.verbraeck@gmail.com>
2026-05-07 23:37:27 +0200

    Issue #4. Demo of use of different locales

--- src/main/java/org/djunits/demo/examples/LocaleDemo.java
+++ src/main/java/org/djunits/demo/examples/LocaleDemo.java
@@ -2,25 +2,24 @@ package org.djunits.demo.examples;
 
 import java.util.Locale;
 
-import org.djunits.old.unit.DurationUnit;
-import org.djunits.old.unit.SpeedUnit;
-import org.djunits.old.unit.util.UNITS;
-import org.djunits.old.value.vdouble.scalar.Duration;
-import org.djunits.old.value.vdouble.scalar.Length;
-import org.djunits.old.value.vdouble.scalar.SIScalar;
-import org.djunits.old.value.vdouble.scalar.Speed;
+import org.djunits.formatter.QuantityFormat;
+import org.djunits.quantity.Duration;
+import org.djunits.quantity.Length;
+import org.djunits.quantity.SIQuantity;
+import org.djunits.quantity.Speed;
 
 /**
  * This Java code demonstrates conversions between related unit using DJUNITS.
  * <p>
- * Copyright (c) 2015-2026 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
- * BSD-style license. See <a href="https://djunits.org/docs/license.html">DJUNITS License</a>.
- * @version $Revision: 672 $, $LastChangedDate: 2019-10-18 14:32:01 +0200 (Fri, 18 Oct 2019) $, by $Author: averbraeck $,
- *          initial version 3 sep. 2015 <br>
+ * Copyright (c) 2015-2026 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
+ * for project information <a href="https://djunits.org" target="_blank"> https://djutils.org</a>. The DJUNITS project is
+ * distributed under a three-clause BSD-style license, which can be found at
+ * <a href="https://djunits.org/docs/license.html" target="_blank"> https://djunits.org/docs/license.html</a>.
+ * <br>
  * @author Alexander Verbraeck
  * @author Peter Knoppers
  */
-public final class LocaleDemo implements UNITS
+public final class LocaleDemo
 {
     /** */
     private LocaleDemo()
@@ -36,20 +35,20 @@ public final class LocaleDemo implements UNITS
     {
         System.out.println("\nPrinting US");
         Locale.setDefault(Locale.US);
-        Duration hour = new Duration(3.0, DurationUnit.HOUR);
-        System.out.println(hour.toTextualString());
+        Duration hour = new Duration(3.0, Duration.Unit.h);
+        System.out.println(hour.format(QuantityFormat.defaults().setTextual()));
         System.out.println(hour);
 
         System.out.println("\nPrinting NL");
         Locale.setDefault(Locale.forLanguageTag("NL"));
-        System.out.println(hour.toTextualString());
+        System.out.println(hour.format(QuantityFormat.defaults().setTextual()));
         System.out.println(hour);
 
         System.out.println("\nParsing UK");
         Locale.setDefault(new Locale("en", "UK"));
         Speed speed = Speed.valueOf("14.2 km/h");
-        System.out.println(speed.toTextualString());
-        System.out.println(speed.toDisplayString());
+        System.out.println(speed.format(QuantityFormat.defaults().setTextual()));
+        System.out.println(speed.format(QuantityFormat.defaults().setDisplay()));
         System.out.println(speed);
 
         try
@@ -65,8 +64,8 @@ public final class LocaleDemo implements UNITS
         System.out.println("\nParsing NL");
         Locale.setDefault(new Locale("nl", "NL"));
         speed = Speed.valueOf("14,2 km/u");
-        System.out.println(speed.toTextualString());
-        System.out.println(speed.toDisplayString());
+        System.out.println(speed.format(QuantityFormat.defaults().setTextual()));
+        System.out.println(speed.format(QuantityFormat.defaults().setDisplay()));
         System.out.println(speed);
 
         try
@@ -92,7 +91,8 @@ public final class LocaleDemo implements UNITS
         try
         {
             speed = Speed.valueOf("18,99 km/h");
-            System.out.println("Correctly parsed 18.99 km/h using fallback locale as " + speed.toDisplayString());
+            System.out.println("Correctly parsed 18.99 km/h using fallback locale as "
+                    + speed.format(QuantityFormat.defaults().setDisplay()));
         }
         catch (Exception e)
         {
@@ -101,25 +101,25 @@ public final class LocaleDemo implements UNITS
 
         System.out.println("\nPrinting NL");
         Locale.setDefault(Locale.forLanguageTag("NL"));
-        System.out.println(new Speed(1234455466787.0, SpeedUnit.MILE_PER_HOUR).toDisplayString());
-        System.out.println(new Speed(1230000000000.0, SpeedUnit.MILE_PER_HOUR).toDisplayString());
-        System.out.println(new Speed(0.112, SpeedUnit.MILE_PER_HOUR).toDisplayString());
-        System.out.println(new Speed(0.000112, SpeedUnit.KNOT).toTextualString());
-        System.out.println(new Speed(1E40, SpeedUnit.MILE_PER_HOUR).toDisplayString());
+        System.out.println(new Speed(1234455466787.0, Speed.Unit.mi_h).format(QuantityFormat.defaults().setDisplay()));
+        System.out.println(new Speed(1230000000000.0, Speed.Unit.mi_h).format(QuantityFormat.defaults().setDisplay()));
+        System.out.println(new Speed(0.112, Speed.Unit.mi_h).format(QuantityFormat.defaults().setDisplay()));
+        System.out.println(new Speed(0.000112, Speed.Unit.kt).format(QuantityFormat.defaults().setTextual()));
+        System.out.println(new Speed(1E40, Speed.Unit.mi_h).format(QuantityFormat.defaults().setDisplay()));
 
         System.out.println("\nPrinting US");
         Locale.setDefault(Locale.US);
-        System.out.println(new Speed(1234455466787.0, SpeedUnit.MILE_PER_HOUR).toDisplayString());
-        System.out.println(new Speed(1230000000000.0, SpeedUnit.MILE_PER_HOUR).toDisplayString());
-        System.out.println(new Speed(0.112, SpeedUnit.MILE_PER_HOUR).toDisplayString());
-        System.out.println(new Speed(0.000112, SpeedUnit.KNOT).toTextualString());
-        System.out.println(new Speed(1E40, SpeedUnit.MILE_PER_HOUR).toDisplayString());
+        System.out.println(new Speed(1234455466787.0, Speed.Unit.mi_h).format(QuantityFormat.defaults().setDisplay()));
+        System.out.println(new Speed(1230000000000.0, Speed.Unit.mi_h).format(QuantityFormat.defaults().setDisplay()));
+        System.out.println(new Speed(0.112, Speed.Unit.mi_h).format(QuantityFormat.defaults().setDisplay()));
+        System.out.println(new Speed(0.000112, Speed.Unit.kt).format(QuantityFormat.defaults().setTextual()));
+        System.out.println(new Speed(1E40, Speed.Unit.mi_h).format(QuantityFormat.defaults().setDisplay()));
 
         System.out.println("\nPrinting SI value NL");
         Locale.setDefault(Locale.forLanguageTag("NL"));
-        Duration d = Duration.valueOf("10,0 uur");
+        Duration d = Duration.valueOf("10,0 u");
         Length l = Length.valueOf("5,0 km");
-        SIScalar pace = SIScalar.divide(d, l);
+        SIQuantity pace = d.divide(l);
         System.out.println("pace has as unit " + pace.getDisplayUnit().toString());
         System.out.println("pace = " + pace.toString());
 
@@ -128,5 +128,13 @@ public final class LocaleDemo implements UNITS
         System.out.println("pace has as unit " + pace.getDisplayUnit().toString());
         System.out.println("pace = " + pace.toString());
 
+        // check the no-NO locale
+        System.out.println("\nPrinting SI value no-NO");
+        Locale.setDefault(Locale.forLanguageTag("no-NO"));
+        Duration dNo = Duration.valueOf("10,0 h");
+        Length lNo = Length.valueOf("5,0 km");
+        pace = dNo.divide(lNo);
+        System.out.println("pace has as unit " + pace.getDisplayUnit().toString());
+        System.out.println("pace = " + pace.toString());
     }
 }
