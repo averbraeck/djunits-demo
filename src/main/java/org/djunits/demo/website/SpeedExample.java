7e775c4d300dc557aae7d1df8d23adbf995e72f3
Alexander Verbraeck <a.verbraeck@gmail.com>
2026-05-07 22:34:20 +0200

    Issue #4. Update the SpeedExample for README.md and index.md

--- src/main/java/org/djunits/demo/website/SpeedExample.java
+++ src/main/java/org/djunits/demo/website/SpeedExample.java
@@ -1,46 +1,18 @@
 package org.djunits.demo.website;
 
-import org.djunits.quantity.Acceleration;
-import org.djunits.quantity.Duration;
-import org.djunits.quantity.Energy;
-import org.djunits.quantity.Length;
-import org.djunits.quantity.Mass;
 import org.djunits.quantity.Speed;
 
 /**
- * SpeedExample.java. <br>
+ * SpeedExample for the index.md page. <br>
  * <br>
  * Copyright (c) 2024-2026 Delft University of Technology, Jaffalaan 5, 2628 BX Delft, the Netherlands. All rights reserved. See
- * for project information <a href="https://djutils.org" target="_blank"> https://djutils.org</a>. The DJUTILS project is
+ * for project information <a href="https://djunits.org" target="_blank"> https://djunits.org</a>. The DJUNITS project is
  * distributed under a three-clause BSD-style license, which can be found at
- * <a href="https://djutils.org/docs/license.html" target="_blank"> https://djutils.org/docs/license.html</a>. <br>
- * @author <a href="https://www.tudelft.nl/averbraeck">Alexander Verbraeck</a>
+ * <a href="https://djunits.org/docs/license.html" target="_blank"> https://djunits.org/docs/license.html</a>. <br>
+ * @author Alexander Verbraeck
  */
 public class SpeedExample
 {
-    /** */
-    private static void example()
-    {
-        Speed speed = new Speed(12, Speed.Unit.km_h);
-        Length length = new Length(4, Length.Unit.mi);
-
-        // Good:
-        Duration howLongOK = length.divide(speed);
-
-        // Does not compile; result would be a frequency:
-        // Duration howLongWrong = speed.divide(length);
-
-        // Does not compile; subtracting a length from a speed makes no sense:
-        // Speed other = speed.subtract(length);
-
-        // Throws a UnitRuntimeException:
-        // Acceleration acceleration = speed.multiply(speed).as(Acceleration.Unit.m_s2);
-
-        // OK:
-        Energy kineticEnergy = speed.multiply(speed).multiply(new Mass(3, Mass.Unit.kg).scaleBy(0.5)).as(Energy.Unit.J);
-        System.out.println(kineticEnergy);
-    }
-
     /** */
     private static void speed()
     {
@@ -50,40 +22,19 @@ public class SpeedExample
         System.out.println("speed2:     " + speed2);
         Speed diff = speed1.subtract(speed2);
 
-        // Default display unit will be SI unit for speed:
+        // Display unit after 'subtract' will be the unit of speed1:
         System.out.println("difference: " + diff);
 
         // Change default display unit; internal SI value is unaltered:
         diff.setDisplayUnit(Speed.Unit.mi_s);
         System.out.println("difference: " + diff);
 
-        // Works, but not mistake-safe:
-        System.out.println("difference: " + diff.getInUnit(Speed.Unit.kt) + " kt");
+        // Works, but error-prone and not localizable:
+        System.out.println("error-prone " + diff.getInUnit(Speed.Unit.kt) + " kt");
 
-        // Safer:
-        System.out.println("difference: " + diff.toString(Speed.Unit.kt));
-
-        // Programmer must be really sure that SI-unit is m/s:
-        System.out.println("difference: " + diff.si() + " m/s (si)");
-
-        // Safer:
-        System.out.println("difference: " + diff.toString(Speed.Unit.SI) + " (si)");
-        System.out.println("difference: " + diff.toString(Speed.Unit.km_h));
-    }
-    
-    /** */
-    private static void example2()
-    {
-        Duration s = Duration.of(20.0, "s");
-        var s2 = s.multiply(s);
-        var l = Length.of(10.0, "m");
-        var a1 = l.divide(s2); // SIQuantity
-        System.out.println("a1 = " + a1);
-        System.out.println("a1.class = " + a1.getClass().getSimpleName());
-        var a2 = a1.as(Acceleration.Unit.m_s2);
-        System.out.println("a2 = " + a2);
-        System.out.println("a2.class = " + a2.getClass().getSimpleName());
-        var a3 = s2.as(Acceleration.Unit.m_s2);
+        // Safer, the unit is provided by the system and localizable:
+        System.out.println("difference: " + diff.format(Speed.Unit.kt));
+        System.out.println("difference: " + diff.format(Speed.Unit.km_h));
     }
     
     /**
@@ -91,7 +42,7 @@ public class SpeedExample
      */
     public static void main(final String[] args)
     {
-        example2();
+        speed();
     }
 
 }
--- src/main/java/org/djunits/demo/website/package-info.java
+++ src/main/java/org/djunits/demo/website/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Examples from the website.
+ * Examples for the website.
  * <p>
  * Copyright (c) 2013-2026 Delft University of Technology, PO Box 5, 2600 AA, Delft, the Netherlands. All rights reserved. <br>
  * BSD-style license. See <a href="https://djunits.org/docs/license.html">DJUNITS License</a>.
